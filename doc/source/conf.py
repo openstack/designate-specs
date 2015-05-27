10c946de50996b0e2b788276cde980829e046a27
Joe Gordon <joe.gordon0@gmail.com>
2015-05-27 15:15:19 -0700

    Drop incubating theme from docs
    OpenStack has dropped the incubation notion, so labeling this as an
    incubated project in the docs is confusing.
    
    Change-Id: I1cdcecc4974ab248cae1a9cccdb587e7256cc993

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -81,7 +81,7 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'incubating': True}
+html_theme_options = {}
 
 
 # Output file base name for HTML help builder.
