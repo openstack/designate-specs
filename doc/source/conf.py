99d49ceb6906fe11853c2c4f340d0190b90a1b0e
Graham Hayes <gr@ham.ie>
2019-04-01 12:26:06 +0100

    Update to openstackdocstheme + sphinx 2.0
    Change-Id: I8f24fd878d034ed240ef5574afaafe1b4adb113b
    Signed-off-by: Graham Hayes <gr@ham.ie>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ import sys
 extensions = [
     'sphinx.ext.autodoc',
     'yasfb',
-    'oslosphinx'
+    'openstackdocstheme',
 ]
 
 exclude_patterns = [
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # html_static_path = ['static']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
